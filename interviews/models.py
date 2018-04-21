a377db23a0e03c605f9ca426ce9ea6ca1b70beb5
Stashkov, Vladimir <testct2@gmail.com>
2018-04-21 13:20:14 +0200

    replace id with name

--- interviews/models.py
+++ interviews/models.py
@@ -3,8 +3,8 @@ from django.db import models
 
 # Create your models here.
 class EmployeeAvailability(models.Model):
-    id = models.AutoField(pk=True)
-    employee_id = models.IntegerField()
+    id = models.AutoField(primary_key=True, )
+    employee_name = models.CharField(max_length=20)
     start_date = models.DateTimeField()
     end_date = models.DateTimeField()
 
