e3fd5317fe02da9cfd0e1acb20a1d8b58664614e
Stashkov, Vladimir <testct2@gmail.com>
2018-04-21 12:42:21 +0200

    add identity fields

--- interviews/models.py
+++ interviews/models.py
@@ -3,16 +3,13 @@ from django.db import models
 
 # Create your models here.
 class EmployeeAvailability(models.Model):
+    id = models.AutoField(pk=True)
+    employee_id = models.IntegerField()
     start_date = models.DateTimeField()
     end_date = models.DateTimeField()
 
     def __str__(self):
         pass  # TODO ID, date, time start, time end
-
-
-class CandidateAvailability(models.Model):
-    start_date = models.DateTimeField()
-    end_date = models.DateTimeField()
 
 
 
