99a2f82ece88bfdebf980eb3eaf3184012749514
Stashkov, Vladimir <testct2@gmail.com>
2018-04-23 00:45:43 +0200

    add todo

--- interviews/models.py
+++ interviews/models.py
@@ -14,6 +14,7 @@ class EmployeeAvailability(models.Model):
     employee = models.ForeignKey(Employee, related_name='timeslots', on_delete=models.CASCADE)
     start_date = models.DateTimeField()
     end_date = models.DateTimeField()
+    # TODO primary key should be a composite key of employee_id, start_date, end_date
 
     def __str__(self):
         return f"{self.employee.name} is available on {self.start_date}"
