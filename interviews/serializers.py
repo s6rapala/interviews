7a7362212a8f6da68eb2a4af09f64f42cd45c353
Stashkov, Vladimir <testct2@gmail.com>
2018-04-22 19:26:10 +0200

    remove migrations

---
+++ interviews/serializers.py
@@ -0,0 +1,35 @@
+from rest_framework import serializers
+
+from interviews.models import EmployeeAvailability, Employee
+
+
+class EmployeeAvailabilitySerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = EmployeeAvailability
+        fields = [
+            'id',
+            'start_date',
+            'end_date',
+            'employee_id',
+        ]
+
+
+class EmployeeSerializer(serializers.HyperlinkedModelSerializer):
+    timeslots = EmployeeAvailabilitySerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Employee
+        fields = [
+            'id',
+            'name',
+            'timeslots',
+        ]
+
+
+class EmployeeSerializerList(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Employee
+        fields = [
+            'id',
+            'name',
+        ]
