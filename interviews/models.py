496e5a8b4fe1009b85bae144c46dfd9fce654a45
Stashkov, Vladimir <testct2@gmail.com>
2018-04-23 22:45:26 +0200

    change str representation for employee model

--- interviews/models.py
+++ interviews/models.py
@@ -8,13 +8,20 @@ class Employee(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=20)
 
+    def __str__(self):
+        return f'id:{self.id} name:{self.name}'
+
 
 class EmployeeAvailability(models.Model):
     id = models.AutoField(primary_key=True)
     employee = models.ForeignKey(Employee, related_name='timeslots', on_delete=models.CASCADE)
     start_date = models.DateTimeField()
     end_date = models.DateTimeField()
-    # TODO primary key should be a composite key of employee_id, start_date, end_date
+
+    class Meta:
+        unique_together = [
+            ('employee', 'start_date', 'end_date'),
+        ]
 
     def __str__(self):
-        return f"{self.employee.name} is available on {self.start_date}"
+        return f'{self.employee.name} is available from {self.start_date:%b-%d %H:%M} to {self.end_date:%b-%d %H:%M}'
